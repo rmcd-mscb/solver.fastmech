4eadf4283bf66a3f2ae8b5908336a494a98e5c83
rmcd <rmcd@localhost.localdomain>
2018-07-26 11:28:19 -0600

    think I fixed CMakelists.txt, congigured, generated and maked, but errors

---
+++ _build/CMakeFiles/Fastmech_iRICv3.dir/cmake_clean_Fortran.cmake
@@ -0,0 +1,114 @@
+# Remove fortran modules provided by this target.
+FILE(REMOVE
+  "calccond.mod"
+  "CALCCOND.mod"
+  "CMakeFiles/Fastmech_iRICv3.dir/calccond.mod.stamp"
+
+  "csedmod.mod"
+  "CSEDMOD.mod"
+  "CMakeFiles/Fastmech_iRICv3.dir/csedmod.mod.stamp"
+
+  "csedmod_dt_susp.mod"
+  "CSEDMOD_DT_SUSP.mod"
+  "CMakeFiles/Fastmech_iRICv3.dir/csedmod_dt_susp.mod.stamp"
+
+  "einitmod.mod"
+  "EINITMOD.mod"
+  "CMakeFiles/Fastmech_iRICv3.dir/einitmod.mod.stamp"
+
+  "geometrymod.mod"
+  "GEOMETRYMOD.mod"
+  "CMakeFiles/Fastmech_iRICv3.dir/geometrymod.mod.stamp"
+
+  "gridcond.mod"
+  "GRIDCOND.mod"
+  "CMakeFiles/Fastmech_iRICv3.dir/gridcond.mod.stamp"
+
+  "gridcoord.mod"
+  "GRIDCOORD.mod"
+  "CMakeFiles/Fastmech_iRICv3.dir/gridcoord.mod.stamp"
+
+  "iriclibf.mod"
+  "IRICLIBF.mod"
+  "CMakeFiles/Fastmech_iRICv3.dir/iriclibf.mod.stamp"
+
+  "median.mod"
+  "MEDIAN.mod"
+  "CMakeFiles/Fastmech_iRICv3.dir/median.mod.stamp"
+
+  "netpreismain.mod"
+  "NETPREISMAIN.mod"
+  "CMakeFiles/Fastmech_iRICv3.dir/netpreismain.mod.stamp"
+
+  "paramsetmod.mod"
+  "PARAMSETMOD.mod"
+  "CMakeFiles/Fastmech_iRICv3.dir/paramsetmod.mod.stamp"
+
+  "readcgns.mod"
+  "READCGNS.mod"
+  "CMakeFiles/Fastmech_iRICv3.dir/readcgns.mod.stamp"
+
+  "rivcalcinitcond.mod"
+  "RIVCALCINITCOND.mod"
+  "CMakeFiles/Fastmech_iRICv3.dir/rivcalcinitcond.mod.stamp"
+
+  "rivconnectivitymod.mod"
+  "RIVCONNECTIVITYMOD.mod"
+  "CMakeFiles/Fastmech_iRICv3.dir/rivconnectivitymod.mod.stamp"
+
+  "rivroughnessmod.mod"
+  "RIVROUGHNESSMOD.mod"
+  "CMakeFiles/Fastmech_iRICv3.dir/rivroughnessmod.mod.stamp"
+
+  "rivstagr4mod_jmn.mod"
+  "RIVSTAGR4MOD_JMN.mod"
+  "CMakeFiles/Fastmech_iRICv3.dir/rivstagr4mod_jmn.mod.stamp"
+
+  "rivvarmod.mod"
+  "RIVVARMOD.mod"
+  "CMakeFiles/Fastmech_iRICv3.dir/rivvarmod.mod.stamp"
+
+  "rivvartimemod.mod"
+  "RIVVARTIMEMOD.mod"
+  "CMakeFiles/Fastmech_iRICv3.dir/rivvartimemod.mod.stamp"
+
+  "rivvarvertmod.mod"
+  "RIVVARVERTMOD.mod"
+  "CMakeFiles/Fastmech_iRICv3.dir/rivvarvertmod.mod.stamp"
+
+  "rivvarwmod.mod"
+  "RIVVARWMOD.mod"
+  "CMakeFiles/Fastmech_iRICv3.dir/rivvarwmod.mod.stamp"
+
+  "rivvertmod.mod"
+  "RIVVERTMOD.mod"
+  "CMakeFiles/Fastmech_iRICv3.dir/rivvertmod.mod.stamp"
+
+  "stpbckinputmod.mod"
+  "STPBCKINPUTMOD.mod"
+  "CMakeFiles/Fastmech_iRICv3.dir/stpbckinputmod.mod.stamp"
+
+  "stpbckoutputmod.mod"
+  "STPBCKOUTPUTMOD.mod"
+  "CMakeFiles/Fastmech_iRICv3.dir/stpbckoutputmod.mod.stamp"
+
+  "stressdivmod.mod"
+  "STRESSDIVMOD.mod"
+  "CMakeFiles/Fastmech_iRICv3.dir/stressdivmod.mod.stamp"
+
+  "support.mod"
+  "SUPPORT.mod"
+  "CMakeFiles/Fastmech_iRICv3.dir/support.mod.stamp"
+
+  "tridiag.mod"
+  "TRIDIAG.mod"
+  "CMakeFiles/Fastmech_iRICv3.dir/tridiag.mod.stamp"
+
+  "uinitmod.mod"
+  "UINITMOD.mod"
+  "CMakeFiles/Fastmech_iRICv3.dir/uinitmod.mod.stamp"
+
+  "writecgns.mod"
+  "WRITECGNS.mod"
+  "CMakeFiles/Fastmech_iRICv3.dir/writecgns.mod.stamp"
+  )
